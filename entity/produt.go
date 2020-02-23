79d8aa8f8100cd891f98277938eddf86c74317c8
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-23 18:15:10 -0300

    Change product entity to add db struct anotation

--- entity/produt.go
+++ entity/produt.go
@@ -3,7 +3,7 @@ package entity
 // Product entity.
 type Product struct {
 	ID           string
-	Title        string
-	Description  string
-	PriceInCents int32 `db:"price_in_cents"`
+	Title        string `db:"title"`
+	Description  string `db:"description"`
+	PriceInCents int32  `db:"price_in_cents"`
 }
