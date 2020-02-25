5d0719b444ddce249f315af33f2d5d65e5a708d6
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-25 17:19:35 -0300

    Add create product use case

--- usecase/product.go
+++ usecase/product.go
@@ -40,6 +40,21 @@ func (u *ProductUseCase) List(ctx context.Context) ([]*entity.Product, error) {
 	return products, nil
 }
 
+// Create product.
+func (u *ProductUseCase) Create(ctx context.Context, p *entity.Product) (*entity.Product, error) {
+	ctx, span := trace.StartSpan(ctx, "usecase.ProductUseCase.List")
+	defer span.End()
+	product, err := u.DataStore.Create(ctx, p)
+	if err != nil {
+		wrappedErr := errors.Wrapf(
+			err,
+			"struct=usecase.ProductUseCase, method=Create, error=repository_create_failed",
+		)
+		return nil, wrappedErr
+	}
+	return product, nil
+}
+
 //NewProductUseCase create a product use case instance.
 func NewProductUseCase(dataStore product.DataStore) *ProductUseCase {
 	return &ProductUseCase{DataStore: dataStore}
--- usecase/product_test.go
+++ usecase/product_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"wiliam.dev/product/entity"
@@ -14,13 +15,38 @@ func TestProductUseCase(t *testing.T) {
 	mockedDataStore := mocks.DataStore{}
 	useCase := NewProductUseCase(&mockedDataStore)
 
-	t.Run("Test List", func(t *testing.T) {
+	t.Run("Test list", func(t *testing.T) {
 		ctx := context.TODO()
 		products := []*entity.Product{}
 		mockedDataStore.On("List", mock.AnythingOfType("*context.valueCtx")).Return([]*entity.Product{}, nil)
 		productsFromDataStore, err := useCase.List(ctx)
 		assert.Nil(t, err)
 		assert.Equal(t, products, productsFromDataStore)
+	})
+
+	t.Run("Test create", func(t *testing.T) {
+		ctx := context.TODO()
+		product := entity.Product{
+			Title:        "Mussum Ipsum",
+			Description:  "cacilds vidis litro abertis",
+			PriceInCents: 10,
+		}
+		mocked := entity.Product{
+			ID:           uuid.NewV4().String(),
+			Title:        "Mussum Ipsum",
+			Description:  "cacilds vidis litro abertis",
+			PriceInCents: 10,
+		}
+		expected := entity.Product{
+			ID:           mocked.ID,
+			Title:        "Mussum Ipsum",
+			Description:  "cacilds vidis litro abertis",
+			PriceInCents: 10,
+		}
+		mockedDataStore.On("Create", mock.AnythingOfType("*context.valueCtx"), mock.Anything).Return(&mocked, nil)
+		productFromDataStore, err := useCase.Create(ctx, &product)
+		assert.Nil(t, err)
+		assert.Equal(t, &expected, productFromDataStore)
 	})
 }
 
